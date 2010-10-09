6fef85558fe3883b07a1b546b8d10dfb0d31faf2
nat7gi <nat7gi@21dc39e3-f6f8-6011-d126-7e1de084ed49>
2010-10-09 21:46:55 +0000

    - NodoEstado: Nuevo atrituto costo_estimado, en Avara seria el valor de aplicarH y en A*, costo + h. No esta inicializado en el constructor. - Busqueda: Funcionsimba aplicarH1... que creo que no sirve porque le entra un NodoEstado n que es el que estoy creando en crearHijos ... muy inteligente xD! A_estrella: copy-paste del metodo que toca sobreescribir, crearHijo

--- src/agentesia/A_estrella.java
+++ src/agentesia/A_estrella.java
@@ -5,6 +5,8 @@
 
 package agentesia;
 
+import java.math.*;
+
 /**
  *
  * @author Administrator
@@ -16,6 +18,116 @@ public class A_estrella extends Busqueda{
     public A_estrella(NodoEstado raiz, int cordX, int cordY, int escen[][])
     {
         nodoRaiz=raiz;
+        cordXSalida=cordX;
+        cordYSalida=cordY;
+        //comparador= new ComparadorEstados();
+        //colaPrioridad= new PriorityQueue(1,comparador);
+        mapa=escen.clone();
+    }
+
+    //copy paste... como carajos aplico heuristica al nodo hijo si apenas lo estoy creando
+    @Override
+    public NodoEstado crearHijo(NodoEstado padre, String operador, int cordX_hijo, int cordY_hijo)
+    {
+        String ruta; //Ruta que guardara la ruta del padre y al padre
+        double costo; //Costo guardará el costo de avanzar al hijo
+        double costo_e;
+        NodoEstado hijo=null; //Nodo estado que se va a retornar
+
+        try{
+
+        //Si la cadenas estan vacias no usar comas
+        if(padre.getOperador().equals("")) operador=padre.getOperador()+operador;
+        else operador=padre.getOperador()+","+operador;
+        if(padre.getRuta().equals("")) ruta="("+padre.getX()+","+padre.getY()+")";
+        else ruta=padre.getRuta()+",("+padre.getX()+","+padre.getY()+")";
+        }catch(OutOfMemoryError exc)
+        {
+            System.err.println("LLego al limite de memoria de java este nodo no se expande");
+            System.err.println("Nodo: "+padre.getX()+","+padre.getY());
+            System.err.println("Profundidad: "+padre.getProfundidadPorOps());
+            System.out.println("Operadores: "+padre.getOperador());
+            return null;
+        }
+        //Memoria para saber que lugares ya visite Solo la utilizo para saber si ya recogí un item.
+        int [][] memoriaPadre=padre.getMemoria();
+        int [][] memoria = new int [10][10];
+
+        for (int idy=0;idy<10;idy++)
+        {
+            for (int idx=0;idx<10;idx++)
+            {
+                memoria[idx][idy]=memoriaPadre[idx][idy];
+            }
+        }
+
+        //Si en el mapa es libre
+        if(memoria[cordX_hijo][cordY_hijo]==0 || memoria[cordX_hijo][cordY_hijo]==3)
+        {
+            //Si va en nave
+            if(padre.getNave()>0)
+            {
+                costo=padre.getCosto()+0.5;
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items(), padre.getN_naves(), padre.getNave()-1,memoria);
+            }
+            else
+            {
+                costo=padre.getCosto()+1;
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items(), padre.getN_naves(), 0,memoria);
+            }
+        }
+        //Si en el mapa es nave
+        else if(memoria[cordX_hijo][cordY_hijo]==4 || memoria[cordX_hijo][cordY_hijo]==5)
+        {
+            //Si va en nave
+            if(padre.getNave()>0)
+            {
+                costo=padre.getCosto()+0.5;
+                //Paso como si nada
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items(), padre.getN_naves(), padre.getNave()-1,memoria);
+            }
+            else
+            {
+                costo=padre.getCosto()+1;
+                memoria[cordX_hijo][cordY_hijo]=0;
+                //Lleno combustible
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items(), padre.getN_naves()-1, 10,memoria);
+            }
+        }
+        //Si en el mapa es item
+        else if(memoria[cordX_hijo][cordY_hijo]==6)
+        {
+            memoria[cordX_hijo][cordY_hijo]=0;
+            //Si va en nave
+            if(padre.getNave()>0)
+            {
+                costo=padre.getCosto()+0.5;
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items()-1, padre.getN_naves(), padre.getNave()-1, memoria);
+            }
+            else
+            {
+                costo=padre.getCosto()+1;
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items()-1, padre.getN_naves(), 0, memoria);
+            }
+            //escenario[x][yN]=0;
+        }
+        //Si en el mapa es campo
+        else if(memoria[cordX_hijo][cordY_hijo]==7)
+        {
+            //Si va en nave
+            if(padre.getNave()>0)
+            {
+                costo=padre.getCosto()+0.5+6;
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items(), padre.getN_naves(), padre.getNave()-1, memoria);
+            }
+            else
+            {
+                costo=padre.getCosto()+1+6;
+                hijo= new NodoEstado(ruta,costo,operador,cordX_hijo,cordY_hijo,padre.getN_items(), padre.getN_naves(), 0, memoria);
+            }
+        }
+
+        return hijo;
     }
 
     public NodoEstado ejecutar()
